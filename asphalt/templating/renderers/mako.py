44d44a243e05e6df1675f48b2e3b93ad405cbad4
Alex Grönholm <alex.gronholm@nextday.fi>
2017-04-11 18:21:31 +0300

    Skip filesystem checks by default in production mode with Mako

--- asphalt/templating/renderers/mako.py
+++ asphalt/templating/renderers/mako.py
@@ -11,6 +11,11 @@ class MakoRenderer(TemplateRenderer):
     """
     Renders Mako templates.
 
+    .. note:: By default, the ``filesystem_checks`` argument of
+      :class:`~mako.lookup.TemplateLookup` is set  to the value of the ``__debug__`` variable.
+      This means that in production mode, it will not check for template changes. If a template is
+      edited in production mode, the application must be restarted for the changes to take effect.
+
     :param package_paths: if given, looks up the directories containing the given package and fills
         in the ``directories`` argument for :class:`~mako.lookup.TemplateLookup`. The value will be
         interpreted by :func:`~asphalt.templating.util.package_to_directory`.
@@ -22,6 +27,7 @@ class MakoRenderer(TemplateRenderer):
     def __init__(self, package_paths: Iterable[str] = (), **lookup_options):
         directories = lookup_options.setdefault('directories', [])
         directories.extend(package_to_directory(pkg_path) for pkg_path in package_paths)
+        lookup_options.setdefault('filesystem_checks', __debug__)
         self.lookup = TemplateLookup(**lookup_options)
 
     def render(self, template: str, **vars) -> str:
--- docs/versionhistory.rst
+++ docs/versionhistory.rst
@@ -9,6 +9,7 @@ This library adheres to `Semantic Versioning <http://semver.org/>`_.
 - **BACKWARD INCOMPATIBLE** Renamed the ``asphalt.templating.util`` module to
   ``asphalt.templating.utils``
 - Renderer resources are now added to the context using their actual types as well
+- The Mako renderer now skips filesystem checks by default in production mode
 
 **1.0.0** (2015-05-21)
 
